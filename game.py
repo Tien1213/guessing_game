3b789c98f707e68d18491b97bfbbbc2dde786ff9
Tien Tran <ttran119@student.gsu.edu>
2019-01-08 16:53:59 -0800

    "update code"

--- game.py
+++ game.py
@@ -3,21 +3,20 @@ b = random.randint(1, 101)
 
 attempt = 0
 
-while attempt < 31:
+while attempt < 100:
     a = int(input("enter a number:"))
     attempt = attempt + 1
 
-    if a > b:
-        print("lower!!")
+    if attempt == 100:
+        print("Out of Attempt!!! " + "The Number is: "+ str(b))
 
+    elif a > b:
+        print("lower!! " + "You have " + str(100 - attempt) + " attempts left")
 
     elif a < b:
-        print("higher!!")
-
+        print("higher!! " + "You have " + str(100 - attempt) + " attempts left")
 
     elif a == b:
-        print("correct!!")
+        print("correct!! The Number is: " + str(b))
         break
-
-
 
